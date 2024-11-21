111648d68238dbf8c68ea1371b3bd688d93e1f96
Sifiso Hadebe <ST10131148@rcconnect.edu.za>
2024-11-21 12:30:48 +0200

    Alert Added

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,7 +1,11 @@
+import { ReactNode } from "react";
 
-const Alert = () => {
+interface Prop{
+    children: ReactNode;
+}
+const Alert = ({children}: Prop) => {
   return (
-    <div>Alert</div>
+    <div className="alert alert-primary">{children}</div>
   )
 }
 
